4d8de866ad2db1924a494856a275d5456a125123
hesham <hesham.elfakharani@gmail.com>
2025-06-26 23:10:25 +0300

    fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.statefulclickcounter"
+    namespace = "com.example.celebriylist"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
